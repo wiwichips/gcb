4db001d8b41c8060e7cd78ec8d2b66b70c1d8c60
rafaelwi <rafwiskailnicki@gmail.com>
2020-08-15 19:12:37 -0400

    jpegs are only allowed now

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
     $('#uploadFile').on('change', function(e) {
-        console.log("detected change");
         // Prepare the data to send
         e.preventDefault();
         var form = document.getElementById('uploadForm');
@@ -29,14 +28,20 @@ $(document).ready(function() {
             }
         });
 
+        var form = document.getElementById('uploadFile');
+        var name = form.files[0].name;        
+
+        // If the image that was uploaded is not a jpg, reject it
+        if (name.split('.')[1] != 'jpg' || name.split('.')[1] != 'jpeg') {
+            alert("Error! Cannot process image! Only .jpg and .jpeg images are allowed.");
+            return;
+        }
+
         // Change the source of the placeholder image on the site to the image
         // that was just uploaded
-        var form = document.getElementById('uploadFile');
-        $('#imgToEvaluate').attr("src", 'pic/' + form.files[0].name);
-        var name = form.files[0].name;
+        $('#imgToEvaluate').attr("src", 'pic/' + name);
 
-        // Reset file chosen button
-        //form.value = null;
+
 
         // Once we have the image uploaded and on the page, we can now run the 
         // classification script
@@ -49,6 +54,5 @@ $(document).ready(function() {
                 console.log(data);
             }
         });
-
     });
 });
