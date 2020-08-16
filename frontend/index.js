2323cfa02443b430c2712400464d040de9514128
wiwichips <wpringle@uoguelph.ca>
2020-08-16 12:08:59 -0400

    switched to rafael's front end

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
     $('#uploadFile').on('change', function(e) {
-        console.log("detected change");
         // Prepare the data to send
         e.preventDefault();
         var form = document.getElementById('uploadForm');
@@ -29,14 +28,20 @@ $(document).ready(function() {
             }
         });
 
+        var form = document.getElementById('uploadFile');
+        var name = form.files[0].name;        
+
+        // If the image that was uploaded is not a jpg, reject it
+        if (name.split('.')[1] != 'jpg' && name.split('.')[1] != 'jpeg') {
+            alert("Error! Cannot process image! Only .jpg and .jpeg images are allowed.");
+            return;
+        }
+
         // Change the source of the placeholder image on the site to the image
         // that was just uploaded
-        var form = document.getElementById('uploadFile');
-        $('#imgToEvaluate').attr("src", 'pic/' + form.files[0].name);
-        var name = form.files[0].name;
+        $('#imgToEvaluate').attr("src", 'pic/' + name);
+        $('#imgResults').text("Determining what's in your image...");
 
-        // Reset file chosen button
-        //form.value = null;
 
         // Once we have the image uploaded and on the page, we can now run the 
         // classification script
@@ -46,9 +51,18 @@ $(document).ready(function() {
             data: {filename: name},
 
             success: (data) => {
+                // Display the results on the webpage
                 console.log(data);
+                let results = "<b>" + "Your image contains" + "</b><br />";
+                data.forEach(function(item) {
+                    results += item.className + " (" + (item.probability * 100).toFixed(2) + "%)" + "<br />"
+                });
+                console.log(results);
+                $('#imgResults').html(results);
+                $('#tryAgainButtonDiv').append('<button type="button" onClick="window.location.reload();" id="tryAgainButton" class="btn btn-success">Try Again</button>')
+
+
             }
         });
-
     });
 });
